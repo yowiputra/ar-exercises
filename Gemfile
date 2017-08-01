a36fd51a4330a6fd0a2700a941d2d3e8072eb18f
Yohan Wiputra <yowiputra@gmail.com>
2017-08-01 20:51:41 +0000

    finished exercises 1 and 2

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@
 source "https://rubygems.org"
 
 gem 'pry' # for debugging
-gem 'pg'
+gem 'pg','~> 0.20'
 gem 'activerecord'
--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby_store = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond_store = Store.create(name: "Richmond", annual_revenue: 1260000, womens_apparel: true)
+gastown_store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true)
+
+puts Store.count
